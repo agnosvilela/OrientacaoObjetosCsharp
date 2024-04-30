f8bdfb9e8e87450516c9b580e7ffab8d5a8683c2
agnoslima <agnos.nl@gmail.com>
2024-04-29 23:14:09 -0300

    Trabalhando com Classes em C#

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Trabalhando com Classes em c#.
+// criando nossa primeira classe.
+using System;
+namespace TrabalhandoComClasses
+{
+
+
+    class Aluno
+    {
+        public string Nome;
+        public int Idade;
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            // instancia da classe é um objeto
+            var aluno1 = new Aluno();
+            aluno1.Nome = "Agnos Vilela";
+            aluno1.Idade = 32;
+
+            Console.WriteLine(aluno1.Nome);
+            Console.WriteLine(aluno1.Idade);
+        }
+    }
+}
