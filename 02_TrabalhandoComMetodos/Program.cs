ee9d8592e718ac04b093847e0bcdaa2354e12696
agnoslima <agnos.nl@gmail.com>
2024-05-01 22:45:57 -0300

    trabalhando com metodos sem retorno, com retorno e que recebem parametros

--- 02_TrabalhandoComMetodos/Program.cs
+++ 02_TrabalhandoComMetodos/Program.cs
@@ -1,5 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-// tralhando com metodos em csharp POO.
+﻿using System;
+namespace TrabalhandoComMetodos
+{
+
+    class ContaCorrente
+    {
+        public string Numero;
+        public decimal Saldo;
+        // metodo sem retorno
+        public void ImprimirInformacoes()
+        {
+            Console.WriteLine($"Numero {Numero} e o saldo é:  {Saldo}  ");
+        }
+        // metodo com retorno
+        public string ListarInfo()
+        {
+            return $" {Numero} {Saldo}";
+        }
+        // metodo que recebe informaçoes nos parametros
+
+        public void Depositar(decimal valor)
+        {
+            Saldo += valor;
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var conta1 = new ContaCorrente();
+            conta1.Numero = "001-002";
+            conta1.Saldo = 0;
+            //conta1.ImprimirInformacoes(); metodo sem retorno
+            Console.WriteLine(conta1.ListarInfo()); // metodo com retorno
+            conta1.Depositar(50);
+            Console.WriteLine(conta1.ListarInfo());
+        }
+
+    }
 
 
-Console.WriteLine("Hello, World!");
+}
