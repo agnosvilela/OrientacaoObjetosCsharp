bdbe77d88986be6f123ad76a8ba6385ec481d8e6
agnoslima <agnos.nl@gmail.com>
2024-05-06 22:57:36 -0300

    Trabalhando com Exceçoes bloco try catch

---
+++ 04_TrabalhandoComExcecoesCsharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace blocoTryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var nomes = new[] { "Agnos", "Suelen" };
+                for (var i = 0; i <= nomes.Length; i++)
+                {
+                    Console.WriteLine(nomes[i]);
+                }
+            }
+            catch (Exception tipoErro)
+            {
+                Console.WriteLine("Ocorreu um erro" + tipoErro.Message);
+
+
+            }
+        }
+    }
+/*Erro do da compilação do código.
+Unhandled exception. System.IndexOutOfRangeException:
+ Index was outside the bounds of the array.*/
