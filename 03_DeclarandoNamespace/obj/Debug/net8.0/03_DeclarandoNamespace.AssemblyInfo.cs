62b4782a5b432ee5b56483b9c2ff4dcbd739b20d
agnoslima <agnos.nl@gmail.com>
2024-05-30 18:00:51 -0300

    classes e metodos

--- 03_DeclarandoNamespace/obj/Debug/net8.0/03_DeclarandoNamespace.AssemblyInfo.cs
+++ 03_DeclarandoNamespace/obj/Debug/net8.0/03_DeclarandoNamespace.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("03_DeclarandoNamespace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45a648adaf615470a77660af10143a4d1f39a45d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7b9e7cd210f218d61963558ce8ec3bd0d009dba")]
 [assembly: System.Reflection.AssemblyProductAttribute("03_DeclarandoNamespace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("03_DeclarandoNamespace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
